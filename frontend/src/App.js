2e7453f9a27380be70d5f2f16d6c711307af8b8b
StEugen <85479518+StEugen@users.noreply.github.com>
2023-03-11 07:59:50 +0000

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,8 @@
 import { useState, useEffect } from 'react'
 
 function App() {
-  const [data, setData] = useState(null);
 
-  useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
-  })
   return (
-    <div>
-      <p>{!data ? "Loading..." : data}</p>
-    </div>
   );
 }
 
