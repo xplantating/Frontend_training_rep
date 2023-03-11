c7691537b3fc160b8d89cc6f6961b0ac09ae0a78
StEugen <85479518+StEugen@users.noreply.github.com>
2023-03-11 07:14:51 +0000

    Making backend

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,14 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+
+
+app.get("/api", (req, res) => {
+    res.json({ message: "Hello world" })
+})
+
+
+
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`)
 })
