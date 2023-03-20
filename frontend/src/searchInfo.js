996c8e90acdda3271747164bd6c8705fbd6515c2
xplantating <113053449+xplantating@users.noreply.github.com>
2023-03-20 20:21:33 +0400

    changes
    update

---
+++ frontend/src/searchInfo.js
@@ -0,0 +1,28 @@
+export const films = [
+  {
+    name: "Transformers",
+    year: "2007",
+    rating: "10/10"
+  },
+  {
+    name: "Transformers revenge of the fallen",
+    year: "2009",
+    rating: "10/10"
+  },
+  {
+    name: "Transformers 3 dark side of the moon",
+    year: "2011",
+    rating: "6/10"
+  },
+  {
+    name: "Transformers 4 Era of extermination",
+    year: "2014",
+    rating: "5/10"
+  },
+  {
+    name: "Transformers 5 the last knight",
+    year: "2009",
+    rating: "7/10"
+  }
+
+]
