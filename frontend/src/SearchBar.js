22f7b524a0c8b8b091a6e1ee41ea6a48c7345069
xplantating <113053449+xplantating@users.noreply.github.com>
2023-03-31 23:04:34 +0400

    searchbar
    done

--- frontend/src/SearchBar.js
+++ frontend/src/SearchBar.js
@@ -2,12 +2,10 @@ import {films} from "./searchInfo"
 import React, {useState} from 'react'
 import './App.css'
 import { Container } from "@mui/system"
-import { styled, alpha } from '@mui/material/styles';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import InputBase from '@mui/material/InputBase';
 import SearchIcon from '@mui/icons-material/Search';
 
 
@@ -16,67 +14,26 @@ export default function SearchBar() {
   console.log(films)
   const [searchTerm, setSearchTerm] = useState("");
   console.log(searchTerm)
-  const handleChange = (event) => {
-  setSearchTerm(event.target.value);
+  const Change = (event) => {
+    setSearchTerm(event.target.value);
   };
 
-  const Search = styled('div')(({ theme }) => ({
-    position: 'relative',
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: alpha(theme.palette.common.white, 0.15),
-    '&:hover': {
-      backgroundColor: alpha(theme.palette.common.white, 0.25),
-    },
-    marginLeft: 0,
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      marginLeft: theme.spacing(1),
-      width: 'auto',
-    },
-  }));
-  
-  const SearchIconWrapper = styled('div')(({ theme }) => ({
-    padding: theme.spacing(0, 2),
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  }));
-  
-  const StyledInputBase = styled(InputBase)(({ theme }) => ({
-    color: 'inherit',
-    '& .MuiInputBase-input': {
-      padding: theme.spacing(1, 1, 1, 0),
-      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-      transition: theme.transitions.create('width'),
-      width: '100%',
-      [theme.breakpoints.up('sm')]: {
-        width: '12ch',
-        '&:focus': {
-          width: '20ch',
-        },
-      },
-    },
-  }));
+
   return (
-    <Container>
+    <Container sx={{mt: 10}}>
     <Box sx={{ flexGrow: 1 }}>
     <AppBar position="static">
       <Toolbar>
-        <Search>
-          <SearchIconWrapper>
-            <SearchIcon />
-          </SearchIconWrapper>
-          <StyledInputBase
-            placeholder="Search"
-            onChange={handleChange}
-            type="text"
-            value={searchTerm}
-            sx={{ width: 600 }}
-          />
-        </Search>
+        <SearchIcon />
+  <input
+    id="text"
+    type="text"
+    placeholder="Search"
+    value={searchTerm}
+    onChange={Change}
+    sx={{ width: 600 }}
+
+/>
       </Toolbar>
     </AppBar>
   </Box>
@@ -95,11 +52,12 @@ export default function SearchBar() {
         </tbody>
 </Container>
   );
-        }
+        } 
 
 
-
-
+        
+ 
+     
 
 
         
